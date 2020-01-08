f75fea2e38f2bb4fb9020400a7c5e51e52be6d65
nokatz <nokacpetrusicki@yahoo.com>
2020-01-09 00:19:41 +0100

    add toggle todo selection

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ form = document.getElementById('form');
 
 
 addBtn.addEventListener('click', showForm);
-// editBtn.addEventListener('click', showForm);
+editBtn.addEventListener('click', showForm);
 
 function showForm(e){
 	
@@ -36,19 +36,48 @@ function showForm(e){
 
 okBtn.addEventListener('click', addTodo);
 
+
 function addTodo(e){
 
 	e.preventDefault();
 
-	todos.push(td);
-	console.log(todos);
+	todos.push(inputField.value);
 	
 	let li = document.createElement('li');
 	li.textContent = inputField.value; 
 	
+	li.addEventListener('click', (e) => {
+		
+		let selected = e.target.classList.contains('selected');
+
+		let items = document.getElementsByTagName('li');
+		
+		for(var i = 0; i < items.length; i++){
+			items[i].classList.remove('selected');
+		}
+		
+		if(selected){
+
+			e.target.classList.remove('selected');
+
+			editBtn.disabled = true;
+			delBtn.disabled = true;
+		
+		} else {
+			
+			e.target.classList.add('selected');
+			
+			editBtn.disabled = false;
+			delBtn.disabled = false;
+		}		
+
+		
+	});
+	
 	list.appendChild(li);
 	
 	form.style.display = 'none';
+	
 }
 
 
